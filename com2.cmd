f16ec62d479ac06d8956d827d88c573e24f2811b
Gogolita <83463247+Gogollita@users.noreply.github.com>
2025-10-02 11:45:08 +0300

    Добавлено пару файлов для тестирования скриптов

---
+++ com2.cmd
@@ -0,0 +1,9 @@
+$HOME
+cd lgfssgs
+ls sfkkld djjd dd
+cd
+jsfjkk
+ls
+cd /home
+ls
+cd $HOME
---
+++ com3.cmd
@@ -0,0 +1,7 @@
+echo "Hello"
+ls
+echo "Script completed"
+cd Users/Desktop
+cd USUSUSUSU
+ls list
+#exit
